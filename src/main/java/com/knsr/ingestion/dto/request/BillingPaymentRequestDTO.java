6cdd6144e636cf5e5afa57e5a34cf5b2c362168f
Sasank Reddy <Sasank Reddy@SASANK_REDDY>
2025-08-05 22:30:33 +0530

    Updated code

--- src/main/java/com/knsr/ingestion/dto/request/BillingPaymentRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.knsr.ingestion.dto.request;
-
-import lombok.Data;
-
-@Data
-public class BillingPaymentRequestDTO {
-    private Long billingId;
-    private String status; // e.g., "PAID"
-}
--- src/main/java/com/knsr/ingestion/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.knsr.ingestion.repository;
-
-import com.knsr.ingestion.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
-
